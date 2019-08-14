3b8780ab4271b51d97163e4c9b1bf47c3b711976
shicks <shicks255@yahoo.com>
2019-08-14 16:14:10 -0500

    need a better way to do this

--- src/CodeWars/EasyWallpaper/EasyWallpaper.kt
+++ src/CodeWars/EasyWallpaper/EasyWallpaper.kt
@@ -11,7 +11,30 @@ fun wallpaper(l:Double, w:Double, h:Double): String {
 
     val rolls = Math.ceil(total/wallpaperTotal)
 
-    return rolls.toString()
+    return when (rolls.toInt()){
+        0 -> "zero"
+        1 -> "one"
+        2 -> "two"
+        3 -> "three"
+        4 -> "four"
+        5 -> "five"
+        6 -> "six"
+        7 -> "seven"
+        8 -> "eight"
+        9 -> "nine"
+        10 -> "ten"
+        11 -> "eleven"
+        12 -> "twelve"
+        13 -> "thirteen"
+        14 -> "fourteen"
+        15 -> "fifteen"
+        16 -> "sixteen"
+        17 -> "seventeen"
+        18 -> "eighteen"
+        19 -> "nineteen"
+        20 -> "twenty"
+        else -> ""
+    }
 }
 
 fun main() {
